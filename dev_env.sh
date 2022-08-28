83530ae8acf4b4ae88b58813c7442d5fdcea800e
David Wheatley <hi@davwheat.dev>
2022-08-28 18:41:07 +0200

    feat: add dev env setup bash script

---
+++ dev_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export API_BASE_URL=https://api.mastdatabase.co.uk
+export DB_HOST=127.0.0.1
+export DB_PORT=3306
+export DB_DATABASE=mastedatabasen_sites
+export DB_USER=mastedatabasen_user
+export DB_PASSWORD=mastedatabasen
+export PORT=8080
+export NODE_ENV=development
